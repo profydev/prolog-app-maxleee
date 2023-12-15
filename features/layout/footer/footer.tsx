ddd36561b6212cfe6cc21d875890ebda96705756
Max <max.schwanger@gmail.com>
2023-12-15 14:49:42 -0500

    footer styling

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -3,7 +3,23 @@ import styles from "./footer.module.scss";
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <h1>footer</h1>;
+      <p className={styles.version}>Version: 14.5.1</p>
+      <div className={styles.footerLinksWrapper}>
+        <a href="#" className={styles.footerLink}>
+          Docs
+        </a>
+        <a href="#" className={styles.footerLink}>
+          API
+        </a>
+        <a href="#" className={styles.footerLink}>
+          Help
+        </a>
+        <a href="#" className={styles.footerLink}>
+          Community
+        </a>
+      </div>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/icons/logo-small.svg" alt="logo" className={styles.logo} />
     </footer>
   );
 }
